4aff5ce75b8f68f6bcd51745d58badf816e074a3
ryonezuuu <ryonezuuu@nakaya-mac.local>
2025-11-14 11:13:10 +0900

    save

--- enshu7_1.m
+++ enshu7_1.m
@@ -1,9 +1,9 @@
 clear all; clc; close all
 
-N=200;
+N=24;
 L=1024;
 dw = pi/(L-1);
-wp = 0.35;
+wp = 0.25;
 pe = ceil(wp*L);
 
 d = [ones(pe, 1); zeros(L-pe, 1)];
@@ -15,4 +15,5 @@ coef = [flipud(a(2:end))/2; a(1); a(2:end)/2];
 
 H = fft(coef, 2*L);
 figure(1); plot(1:length(H(1:L)), abs(H(1:L)), 1:length(H(1:L)), d)
-saveas(figure(1),"img_7_"+N+"_"+wp+".png","png");
+pause;
+saveas(figure(1),strcat("img_7_",num2str(N),"_",num2str(wp),".png"));
---
+++ report1/wlsm_lp.m
@@ -0,0 +1,27 @@
+clear all; clc; close all
+
+N=24;   %fixed
+L=1024; %fixed
+dw = pi/(L-1);
+wp = 0.25;  %fixed
+ws = 0.29;
+
+pe = ceil(wp*L);
+se = ceil(ws*L);
+
+d = [ones(pe, 1); zeros(L-pe, 1)];
+Q = [ones(L, 1) cos((0:L-1)'*(1:(N-1))*dw)];
+
+pm = 0.1;
+sm = 1.0;
+w = [pm*ones(pe, 1); zeros(se - pe, 1); sm*ones(L - se, 1)];
+W = diag(w);
+
+a = (Q'*W^2*Q)\(Q'*W^2*d);
+
+coef = [flipud(a(2:end))/2; a(1); a(2:end)/2];
+
+H = fft(coef, 2*L);
+figure(1); plot(1:length(H(1:L)), abs(H(1:L)), 1:length(H(1:L)), d)
+pause;
+saveas(figure(1), strcat('img_report_lp',"_", num2str(ws),"_",num2str(pm), "_", num2str(sm),'.png'))
